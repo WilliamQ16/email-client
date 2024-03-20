236448bcf831c5b4f304362795d4bc24b43a2623
proyectosena020 <senaproyecto020@gmail.com>
2024-03-20 16:05:45 -0500

    second commit

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mail.urls')),
-    staticfiles_urlpatterns()
+    path('', include('mail.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
